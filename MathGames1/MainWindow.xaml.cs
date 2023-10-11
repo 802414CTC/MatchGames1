871c638e224591d0d2de1b08760235e14c5342c2
802414CTC <802414CTC@MA-CTC-SDD-S29>
2023-10-11 14:04:25 -0400

    Revert "the emojis are done"
    This reverts commit abdcbd26d400dfca0b67d011737be3766e54d23c.

--- MathGames1/MainWindow.xaml.cs
+++ MathGames1/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Threading;
+
 namespace MathGames1
 {
     /// <summary>
@@ -21,28 +20,10 @@ namespace MathGames1
     /// </summary>
     public partial class MainWindow : Window
     {
-        DispatcherTimer timer = new DispatcherTimer();
-        int tenthsOfSecondsElapsed;
-        int matchesFound;
         public MainWindow()
         {
             InitializeComponent();
-            timer.Interval = TimeSpan.FromSeconds(.1);
-            timer.Tick += Timer_Tick;
             SetUpGame();
-        }
-
-        private void Timer_Tick(object? sender, EventArgs e)
-        {
-           
-                tenthsOfSecondsElapsed++;
-                timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
-                if (matchesFound == 8)
-                {
-                    timer.Stop();
-                    timeTextBlock.Text = timeTextBlock.Text + " - Play again?";
-                
-            }
         }
 
         private void SetUpGame()
@@ -55,22 +36,15 @@ namespace MathGames1
                   "👍","👍","👌","👌",
 
             };
-            Random random = new Random();
+            Random  random = new Random();
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                if (textBlock.Name != "timeTextBlock")
-                {
-                    textBlock.Visibility = Visibility.Visible;
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+           }
 
-                    int index = random.Next(animalEmoji.Count);
-                    string nextEmoji = animalEmoji[index];
-                    textBlock.Text = nextEmoji;
-                    animalEmoji.RemoveAt(index);
-                }
-            }
-            timer.Start();
-            tenthsOfSecondsElapsed = 0;
-            matchesFound = 0;
 
         }
 
@@ -78,38 +52,5 @@ namespace MathGames1
         {
 
         }
-        TextBlock lastTextBlockClicked;
-        bool findingMacth = false;
-        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            TextBlock textBlock = sender as TextBlock; if (findingMacth == false)
-            {
-                textBlock.Visibility = Visibility.Hidden;
-                lastTextBlockClicked = textBlock;
-                findingMacth = true;
-            }
-            else if (textBlock.Text == lastTextBlockClicked.Text)
-            {
-                matchesFound++;
-                textBlock.Visibility = Visibility.Hidden;
-                findingMacth = false;
-
-            }
-            else
-            {
-                lastTextBlockClicked.Visibility = Visibility.Visible;
-                findingMacth = false;
-            }
-        }
-
-        private void timeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (matchesFound == 8)
-            {
-                SetUpGame();
-            }
-            }
     }
-    }
-
-
+}
